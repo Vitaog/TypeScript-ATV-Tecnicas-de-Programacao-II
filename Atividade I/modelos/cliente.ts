075d06f51722f826da3eb97f278c0e00f118f66a
Vitor Garcez <vitaoog@gmail.com>
2023-11-08 21:48:04 -0300

    Atividade I Correção Clonar Telefone dependente

--- Atividade I/modelos/cliente.ts
+++ Atividade I/modelos/cliente.ts
@@ -3,18 +3,13 @@ import Endereco from "./endereco"
 import Telefone from "./telefone"
 
 export default class Cliente {
-    public id: number;
     public nome: string
+    public nomeSocial: string
     public dataNascimento: Date
     public dataCadastro: Date
     public telefones: Telefone[] = []
     public endereco: Endereco
     public documentos: Documento[] = []
     public dependentes: Cliente[] = []
-    public titular: Cliente | null
-
-    constructor() {
-        this.id = 0;
-        this.titular = null;
-    }
+    public titular: Cliente
 }
--- Atividade I/teste/index.ts
+++ Atividade I/teste/index.ts
@@ -1,37 +1,35 @@
-import Entrada from "./entrada";
-import ClienteController from "../controller/clienteController";
+import Cliente from "../modelos/cliente";
+import Endereco from "../modelos/endereco";
+import Telefone from "../modelos/telefone";
 
-function menu() {
-    const clienteController = new ClienteController();
-    while (true) {
-        console.log("*********** BEM VINDO AO ATLANTIS! ***********")
-        console.log("Menu:");
-        console.log("1. Cadastrar Cliente");
-        console.log("2. Cadastrar Dependentes");
-        console.log("3. Listar Clientes");
-        console.log("4. Sair");
-        console.log("------------------------------------------------")
-        const entrada = new Entrada();
-        const escolha = entrada.receberNumero("Digite o número com a opção desejada ");
+let cliente = new Cliente()
+cliente.nome = `Pedro de Alcântara João Carlos Leopoldo Salvador`
+cliente.nomeSocial = `Dom Pedro II`
+cliente.dataCadastro = new Date(1840, 6, 23)
+cliente.dataNascimento = new Date(1825, 11, 2)
+let endereco = new Endereco()
+endereco.rua = `R. do Catete`
+endereco.bairro = `Copacabana`
+endereco.cidade = `Rio de Janeiro`
+endereco.estado = `Rio de Janeiro`
+endereco.pais = `Brasil`
+endereco.codigoPostal = `22220-000`
+cliente.endereco = endereco
 
-        switch (escolha) {
-            case 1:
-                clienteController.cadastrarCliente();
-                break;
-            case 2:
-                clienteController.cadastrarDependentes();
-                break;
-            case 3:
-                clienteController.listarClientes();
-                break;
-            case 4:
-                console.log("Saindo do programa.");
-                return;
-            default:
-                console.log("Opção inválida. Tente novamente.");
-                break;
-        }
-    }
-}
+let telefoneCliente = new Telefone()
+telefoneCliente.ddd = "21"
+telefoneCliente.numero = "123456789"
+cliente.telefones.push(telefoneCliente)
 
-menu();
+let dependente = new Cliente()
+dependente.nome = `Isabel Cristina Leopoldina Augusta Micaela`
+dependente.nomeSocial = `Princesa Isabel`
+dependente.dataCadastro = new Date(1921, 10, 14)
+dependente.dataNascimento = new Date(1846, 6, 29)
+dependente.endereco = (cliente.endereco.clonar() as Endereco)
+dependente.telefones.push(telefoneCliente.clonar() as Telefone)
+dependente.titular = cliente
+cliente.dependentes.push(dependente)
+
+console.log(cliente);
+console.log(dependente);
